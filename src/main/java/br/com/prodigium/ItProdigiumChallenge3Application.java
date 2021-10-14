c3d7f4854a83c5f4ff0acdc4eaf84411dceb1c69
Lucas-270 <61168952+Lucas-270@users.noreply.github.com>
2021-10-14 16:46:59 -0300

    Mudanças para nuvem

--- src/main/java/br/com/prodigium/ItProdigiumChallenge3Application.java
+++ src/main/java/br/com/prodigium/ItProdigiumChallenge3Application.java
@@ -3,11 +3,19 @@ package br.com.prodigium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class ItProdigiumChallenge3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItProdigiumChallenge3Application.class, args);
 	}
+	
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(ItProdigiumChallenge3Application.class);
+    	}
 
 }
